be52f229055bc044059873192afc2d8e7677e97a
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-04-16 20:47:02 -0400

    watch list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,16 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks
+  before_destroy :check_for_bookmarks
 
+  private
+
+  def check_for_bookmarks
+    if bookmarks.count.positive?
+      errors.add('cannot delete')
+      return false
+    end
+  end
+
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
 end
